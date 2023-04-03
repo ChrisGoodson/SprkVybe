23239ece66dc0126bd6b2c742393ea779d9864a4
Chris <f.chrisgoodson@icloud.com>
2023-04-03 03:48:03 -0500

    Initial Commit

---
+++ SprkVybe/SprkVybeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprkVybeApp.swift
+//  SprkVybe
+//
+//  Created by Chris on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct SprkVybeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
